1e80f5aa23381b859c9225cbf17776f536dc1b89
zibowang81192 <zibowang81192@gmail.com>
2021-12-31 19:45:28 +0800

    增加了配置项的示例

--- src/main/java/com/wangzb/wiki/controller/TestController.java
+++ src/main/java/com/wangzb/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.wangzb.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,9 @@ import java.util.Map;
 @RestController
 
 public class TestController {
+
+    @Value("${test.hello}")
+    private String testHello;
 //    @RequestMapping("/hello")	支持以上四种请求
 //
 //    @GetMapping("/hello")	只支持Get
@@ -22,9 +26,10 @@ public class TestController {
 //
 //    @RequestMapping(value = "/user/1", method = RequestMethod.GET)只支持GET
 
+
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World!";
+        return "Hello World! "+testHello;
     }
     @PostMapping("/hello/post")
     public String helloPost(String name){
