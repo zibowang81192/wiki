84f4c403e5146829d50c85a9067545de9f520b83
zibowang81192 <zibowang81192@gmail.com>
2022-01-01 16:51:48 +0800

    创建demo表

--- doc/DB/all.sql
+++ doc/DB/all.sql
@@ -1,4 +1,4 @@
-use wiki;
+#建立test表
 drop table if exists test;
 create table test(
                      id bigint not null comment 'id',
@@ -6,3 +6,13 @@ create table test(
                      password varchar(50) comment '密码',
                      primary key (id)
 ) engine=innodb default charset=utf8mb4 comment='测试';
+INSERT INTO test (id, name, password) VALUES (1, '史蒂芬周', '9527');
+
+#建立demo表
+drop table if exists demo;
+create table demo(
+                     id bigint not null comment 'id',
+                     name varchar(50) comment '名称',
+                     primary key (id)
+) engine=innodb default charset=utf8mb4 comment='测试';
+INSERT INTO demo (id, name) VALUES (1, '史蒂芬周');
