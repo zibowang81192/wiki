b13b80af0b8b1b693030d4688c0ee12b71337f54
zibowang81192 <zibowang81192@gmail.com>
2022-01-02 15:12:42 +0800

    制作the-footer组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Wiki ©2022 Created by Zibo Wang
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer',
+  props: {
+    msg: String,
+  },
+});
+</script>
