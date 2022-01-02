11194b5a0ec37d1d99066c60fb87dbe06270853b
zibowang81192 <zibowang81192@gmail.com>
2022-01-02 20:47:38 +0800

    使用axios调用了后端接口 Ebook/list

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -71,8 +71,15 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import axios from 'axios';
 
 export default defineComponent({
   name: 'Home',
+  setup(){
+    console.log("setup");
+    axios.get("http://localhost:8880/Ebook/list?name=Java").then((response)=>{
+      console.log(response);
+    })
+  }
 });
 </script>
