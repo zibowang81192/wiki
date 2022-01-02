14cd0896a86f01f15ecb73b447f21937bc4f3a96
zibowang81192 <zibowang81192@gmail.com>
2022-01-02 15:17:13 +0800

    去除了HelloWorld组件以及相关代码

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -71,12 +71,8 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
 });
 </script>
