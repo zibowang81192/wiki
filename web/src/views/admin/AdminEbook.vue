870b30cb4c93e642584d136c62dfeafbe2b46dbe
zibowang81192 <zibowang81192@gmail.com>
2022-01-04 13:09:50 +0800

    修改电子书管理页面布局

--- web/src/views/admin/AdminEbook.vue
+++ web/src/views/admin/AdminEbook.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="admin-ebook">
-    <h1>Manage Ebooks</h1>
-  </div>
+  <a-layout-content
+      :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+  >
+    <div class="admin-ebook">
+      <h1>Manage Ebooks</h1>
+    </div>
+
+  </a-layout-content>
+
 </template>
