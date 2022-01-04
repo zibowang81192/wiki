68ddc45036672cfaedca00784b02ccfac4fd16dc
zibowang81192 <zibowang81192@gmail.com>
2022-01-04 14:44:40 +0800

    修改了电子书管理页面布局 增加了"vue/no-unused-vars":"off"
    "vue/no-unused-components":"off"
    
    检校规则

--- web/src/views/admin/AdminEbook.vue
+++ web/src/views/admin/AdminEbook.vue
@@ -5,38 +5,25 @@
     <div class="admin-ebook">
       <h1>Manage Ebooks</h1>
     </div>
-    <a-table :columns="columns" :data-source="data">
-      <template #name="{ text }">
-        <a>{{ text }}</a>
+    <a-table :columns="columns"
+             :row-key="record=> record.id"
+             :data-source="ebooks"
+             :pagination="pagination"
+             :loading="loading"
+             @change="handleTableChange"
+    >
+      <template #cover="{text: cover}">
+        <img v-if="cover" :src="cover" alt="avatar">
       </template>
-      <template #customTitle>
-      <span>
-        <smile-outlined />
-        Name
-      </span>
-      </template>
-      <template #tags="{ text: tags }">
-      <span>
-        <a-tag
-            v-for="tag in tags"
-            :key="tag"
-            :color="tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'"
-        >
-          {{ tag.toUpperCase() }}
-        </a-tag>
-      </span>
-      </template>
-      <template #action="{ record }">
-      <span>
-        <a>Invite 一 {{ record.name }}</a>
-        <a-divider type="vertical" />
-        <a>Delete</a>
-        <a-divider type="vertical" />
-        <a class="ant-dropdown-link">
-          More actions
-          <down-outlined />
-        </a>
-      </span>
+      <template v-slot:action="{text, record}">
+        <a-space size="small">
+          <a-button type="primary">
+            编辑
+          </a-button>
+          <a-button type="danger">
+            删除
+          </a-button>
+        </a-space>
       </template>
     </a-table>
 
@@ -45,71 +32,136 @@
 </template>
 
 <script lang="ts">
-import { SmileOutlined, DownOutlined } from '@ant-design/icons-vue';
-import { defineComponent } from 'vue';
-const columns = [
-  {
-    dataIndex: 'name',
-    key: 'name',
-    slots: { title: 'customTitle', customRender: 'name' },
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-  {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    slots: { customRender: 'tags' },
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    slots: { customRender: 'action' },
-  },
-];
+import {defineComponent, onMounted, reactive, ref, toRef} from 'vue';
+import { StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons-vue';
+import axios from 'axios';
 
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
-  },
-];
+const listData: Record<string, string>[] = [];
+
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
 
 export default defineComponent({
+  name: 'AdminEbooks',
+
+  components: {
+    StarOutlined,
+    LikeOutlined,
+    MessageOutlined,
+  },
+
   setup() {
+    console.log("setup");
+    const ebooks = ref();
+    const pagination = {
+      onChange: (page: number) => {
+        console.log(page);
+      },
+      pageSize: 2,
+      total: 0
+    };
+
+    // const pagination = ref({
+    //   current: 1,
+    //   pageSize: 10,
+    //   total: 0
+    // });
+    const loading =ref(false);
+
+    const columns = [
+      {
+      title: 'Cover',
+      dataIndex: 'cover',
+      slots: {customRender: 'cover'}
+      },
+      {
+        title: 'Name',
+        dataIndex: 'name',
+        slots: {customRender: 'name'}
+      },
+      {
+        title: 'Category 1',
+        dataIndex: 'category_id_1',
+        slots: {customRender: 'name'}
+      },
+      {
+        title: 'Category 2',
+        dataIndex: 'category_id_2',
+        slots: {customRender: 'name'}
+      },
+      {
+        title: 'Star',
+        dataIndex: 'star_count'
+      },
+      {
+        title: 'View',
+        dataIndex: 'view_count'
+      },
+      {
+        title: 'Comment',
+        dataIndex: 'comment_count'
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        slots: {customRender: 'action'}
+      }
+    ];
+
+    const handleQuery=(params:any)=>{
+      loading.value = true;
+      axios.get("/Ebook/list").then((response)=>{
+        loading.value = false;
+        const data = response.data;
+        ebooks.value = data.content;
+
+        //pagination.value.current = params.page;
+      });
+    }
+
+    /**
+     *  表格点击页码时触发
+     */
+    const handleTableChange = (pagination: any) => {
+      console.log("看看自带的分页参数都有啥：" + pagination);
+      handleQuery({
+        page: pagination.current,
+        size: pagination.pageSize
+      });
+    };
+
+    // const actions: Record<string, string>[] = [
+    //   { type: 'StarOutlined', text: '156' },
+    //   { type: 'LikeOutlined', text: '156' },
+    //   { type: 'MessageOutlined', text: '7' },
+    // ];
+    onMounted(()=>{
+      console.log("onMounted");
+      handleQuery({});
+    })
+
     return {
-      data,
+      ebooks,
+      pagination,
       columns,
+      loading,
+      handleTableChange,
     };
   },
-  components: {
-    SmileOutlined,
-    DownOutlined,
-  },
 });
+
 </script>
+<style>
+.box {
+  display: flex;
+}
+</style>
