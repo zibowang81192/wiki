8ffba4246f7a63ffaae324269641888279f828c9
zibowang81192 <zibowang81192@gmail.com>
2022-01-19 18:16:12 +0800

    添加了雪花算法工具类 完成新增功能 SQL脚本添加封面路径 存在响应式变量问题待解决

--- doc/DB/all.sql
+++ doc/DB/all.sql
@@ -31,8 +31,8 @@ create table ebook(
     vote_count int comment '点赞数',
     primary key (id)
 ) engine=innodb default charset=utf8mb4 comment='电子书';
-insert into ebook (id, name, description) values(1, 'Spring Boot 入门教程','零基础入门 Java 开发，企业级应用开发最佳首选框架');
-insert into ebook (id, name, description) values(2, 'Vue 入门教程','零基础入门 Vue 开发，企业级应用开发最佳首选框架');
-insert into ebook (id, name, description) values(3, 'Thinking in Java','Java 入门经典数目');
-insert into ebook (id, name, description) values(4, 'Java Core','Java 核心技术，最适合 Java 初学者');
-insert into ebook (id, name, description) values(5, '计算机组成与设计: 硬件/软件接口','计算机专业经典书籍，深入理解计算机的组成');
+insert into ebook (id, name, cover, description) values(1, 'Spring Boot 入门教程', '/image/cover1.png','零基础入门 Java 开发，企业级应用开发最佳首选框架');
+insert into ebook (id, name, cover, description) values(2, 'Vue 入门教程','/image/cover2.png','零基础入门 Vue 开发，企业级应用开发最佳首选框架');
+insert into ebook (id, name, cover, description) values(3, 'Thinking in Java','/image/cover1.png','Java 入门经典数目');
+insert into ebook (id, name, cover, description) values(4, 'Java Core','/image/cover2.png','Java 核心技术，最适合 Java 初学者');
+insert into ebook (id, name, cover, description) values(5, '计算机组成与设计: 硬件/软件接口','/image/cover1.png','计算机专业经典书籍，深入理解计算机的组成');
