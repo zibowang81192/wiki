ef67b11d3b4f3c408c58ab1d849401290351459c
zibowang81192 <zibowang81192@gmail.com>
2022-01-22 14:33:12 +0800

    使用递归算法，将树型数据中当前目标节点以及子孙节点设置成disabled 为树型选择增加一个“无”选择，用来新增一级节点

--- web/src/views/admin/AdminDoc.vue
+++ web/src/views/admin/AdminDoc.vue
@@ -74,6 +74,18 @@
           <a-input v-model:value="doc.name" />
         </a-form-item>
         <a-form-item label="parent">
+          <a-tree-select
+              v-model:value="doc.parent"
+              style="width: 100%"
+              :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
+              :tree-data="treeSelectData"
+              placeholder="Please select parent document"
+              tree-default-expand-all
+              :replaceFields="{title: 'name', key: 'id', value: 'id'}"
+          >
+          </a-tree-select>
+        </a-form-item>
+        <a-form-item label="parent">
           <a-input v-model:value="doc.parent" />
           <a-select
               ref="select"
@@ -167,6 +179,7 @@ export default defineComponent({
 
     const handleQuery=()=>{
       loading.value = true;
+      level1.value = [];
       axios.get("/Doc/all").then((response)=>{
         loading.value = false;
         const data = response.data;
@@ -189,6 +202,8 @@ export default defineComponent({
      *  编辑
      */
     const doc = ref({});
+    const treeSelectData = ref();
+    treeSelectData.value = [];
 
     const modalVisible = ref<boolean>(false);
     const modalLoading = ref<boolean>(false);
@@ -215,11 +230,22 @@ export default defineComponent({
     const edit = (record: any) => {
       modalVisible.value = true;
       doc.value = Tool.copy(record);
+
+      //不能选择自身及其子节点作为父节点
+      treeSelectData.value = Tool.copy(level1.value);
+      setDisable(treeSelectData.value, record.id);
+
+      // 为选择树添加一个“无”
+      treeSelectData.value.unshift({id: 0, name: '无'});
     }
 
     const add = () => {
       modalVisible.value = true;
       doc.value = {};
+      //不能选择自身及其子节点作为父节点
+      treeSelectData.value = Tool.copy(level1.value);
+      // 为选择树添加一个“无”
+      treeSelectData.value.unshift({id: 0, name: '无'});
     }
 
     const handleDelete = (id: number) =>{
@@ -232,6 +258,37 @@ export default defineComponent({
       });
     };
 
+    /**
+     * 将某节点及其子孙节点全部置为disabled
+     */
+    const setDisable = (treeSelectData: any, id: any) => {
+      // console.log(treeSelectData, id);
+      // 遍历数组，即遍历某一层节点
+      for (let i = 0; i < treeSelectData.length; i++) {
+        const node = treeSelectData[i];
+        if (node.id === id) {
+          // 如果当前节点就是目标节点
+          console.log("disabled", node);
+          // 将目标节点设置为disabled
+          node.disabled = true;
+
+          // 遍历所有子节点，将所有子节点全部都加上disabled
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            for (let j = 0; j < children.length; j++) {
+              setDisable(children, children[j].id)
+            }
+          }
+        } else {
+          // 如果当前节点不是目标节点，则到其子节点再找找看。
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            setDisable(children, id);
+          }
+        }
+      }
+    };
+
     onMounted(()=>{
       console.log("onMounted");
       handleQuery();
@@ -242,6 +299,7 @@ export default defineComponent({
       doc,
       //docs,
       level1,
+      treeSelectData,
       columns,
       loading,
       handleQuery,
@@ -252,6 +310,7 @@ export default defineComponent({
       modalLoading,
       handleModalOk,
       handleDelete,
+      setDisable
 
     };
   },
