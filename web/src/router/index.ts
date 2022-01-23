d2364475044d1805c7e50f20c3fcd12a858025dd
zibowang81192 <zibowang81192@gmail.com>
2022-01-23 18:55:05 +0800

    重写用户表设计与持久层代码生成 从电子书表复制增删改查功能

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -18,6 +18,14 @@ const routes: Array<RouteRecordRaw> = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
+    path: '/admin/user',
+    name: 'AdminUser',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/admin/AdminUser.vue')
+  },
+  {
     path: '/admin/ebook',
     name: 'AdminEbook',
     // route level code-splitting
