6fc18b297d88aeaee9d2110e5dc217e469945736
zibowang81192 <zibowang81192@gmail.com>
2022-01-25 00:25:49 +0800

    修改时，不要修改密码

--- src/main/java/com/wangzb/wiki/service/UserService.java
+++ src/main/java/com/wangzb/wiki/service/UserService.java
@@ -93,6 +93,7 @@ public class UserService {
         else{
             //更新
             user.setLoginName(null);
+            user.setPassword(null);
             userMapper.updateByPrimaryKeySelective(user);
         }
 
