35eee9b7e255953699bb2d5d5750b932df971af5
zibowang81192 <zibowang81192@gmail.com>
2022-01-26 12:39:59 +0800

    增加日志

--- src/main/java/com/wangzb/wiki/job/DocJob.java
+++ src/main/java/com/wangzb/wiki/job/DocJob.java
@@ -1,16 +1,17 @@
 package com.wangzb.wiki.job;
- import com.wangzb.wiki.service.DocService;
- import org.slf4j.Logger;
- import org.slf4j.LoggerFactory;
- import org.springframework.scheduling.annotation.Scheduled;
- import org.springframework.stereotype.Component;
 
- import javax.annotation.Resource;
- import java.text.SimpleDateFormat;
- import java.util.Date;
+import com.wangzb.wiki.service.DocService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
- @Component
- public class DocJob {
+import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class DocJob {
 
     private static final Logger LOG = LoggerFactory.getLogger(DocJob.class);
 
@@ -22,9 +23,12 @@ package com.wangzb.wiki.job;
      * 只有等上一次执行完成，下一次才会在下一个时间点执行，错过就错过
      */
     @Scheduled(cron = "5/30 * * * * ?")
-    public void cron(){
+    public void cron() {
+        LOG.info("更新电子书下的文档数据开始");
+        long start = System.currentTimeMillis();
         docService.updateEbookInfo();
+        LOG.info("更新电子书下的文档数据结束, 耗时：{}毫秒", System.currentTimeMillis() - start);
 
     }
 
- }
+}
