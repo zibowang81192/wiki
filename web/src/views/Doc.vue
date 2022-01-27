40b8b21e4fc1710d4c1c1411ea6f215d13204341
zibowang81192 <zibowang81192@gmail.com>
2022-01-28 00:55:34 +0800

    富文本框图片、视频自适应

--- web/src/views/Doc.vue
+++ web/src/views/Doc.vue
@@ -467,4 +467,15 @@ wangeditor 默认样式
     font-size: 16px !important;
     font-weight: 600;
   }
+  /* 图片自适应 */
+  .wangeditor img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  /* 视频自适应 */
+  .wangeditor iframe {
+    width: 100%;
+    height: 400px;
+  }
 </style>
