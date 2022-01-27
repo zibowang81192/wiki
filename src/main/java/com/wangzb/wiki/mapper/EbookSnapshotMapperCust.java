6f96f0b4735354b35d2d2579f71c549e482e3145
zibowang81192 <zibowang81192@gmail.com>
2022-01-27 22:07:30 +0800

    后端增加统计数值接口

--- src/main/java/com/wangzb/wiki/mapper/EbookSnapshotMapperCust.java
+++ src/main/java/com/wangzb/wiki/mapper/EbookSnapshotMapperCust.java
@@ -1,7 +1,13 @@
 package com.wangzb.wiki.mapper;
 
+import com.wangzb.wiki.resp.StatisticResp;
+
+import java.util.List;
+
 public interface EbookSnapshotMapperCust {
 
     public void genSnapshot();
+
+    List<StatisticResp> getStatistic();
 
 }
--- src/main/java/com/wangzb/wiki/service/EbookSnapshotService.java
+++ src/main/java/com/wangzb/wiki/service/EbookSnapshotService.java
@@ -1,9 +1,11 @@
 package com.wangzb.wiki.service;
 
 import com.wangzb.wiki.mapper.EbookSnapshotMapperCust;
+import com.wangzb.wiki.resp.StatisticResp;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class EbookSnapshotService {
@@ -13,6 +15,14 @@ public class EbookSnapshotService {
 
     public void genSnapshot() {
         ebookSnapshotMapperCust.genSnapshot();
+    }
+
+
+    /**
+     * 获取首页数值数据：总阅读数、总点赞数、今日阅读数、今日点赞数、今日预计阅读数、今日预计阅读增长
+     */
+    public List<StatisticResp> getStatistic() {
+        return ebookSnapshotMapperCust.getStatistic();
     }
 
 }
